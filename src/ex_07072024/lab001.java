409cd052d6b4de58e4d3e05f8f731b34b00dbc9c
yugantsethi <yugant.sethi@hotmail.com>
2024-07-07 12:31:58 +0530

    sample

---
+++ src/ex_07072024/lab001.java
@@ -0,0 +1,8 @@
+package ex_07072024;
+
+public class lab001 {
+
+    public static void main(String[] args) {System.out.println("Hello !");
+
+    }
+}
---
+++ src/ex_07072024/lab004.java
@@ -0,0 +1,10 @@
+package ex_07072024;
+
+public class lab004 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2+2);
+        System.out.println(2+2);
+        System.out.println(2+2);
+    }
+}
